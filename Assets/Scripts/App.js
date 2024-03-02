5e667cd1ea19e02f7a98070876a2fa80712297a3
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-03-02 20:29:24 +0330

    style : formatting my code

--- Assets/Scripts/App.js
+++ Assets/Scripts/App.js
@@ -10,16 +10,13 @@ var isTaskEdited = false;
 var itemsId;
 
 function handleActiveFilterClass(filterID) {
-  document
-    .querySelector("span.active-filter")
-    .classList.remove("active-filter");
+  document.querySelector("span.active-filter").classList.remove("active-filter");
   var filterItem = document.getElementById(filterID);
   filterItem.classList.add("active-filter");
 }
 
 input.addEventListener("keyup", function (event) {
   var inputValue = input.value.trim();
-
   if (event.key === "Enter" && inputValue) {
     if (isTaskEdited == false) {
       var item = {
@@ -34,7 +31,6 @@ input.addEventListener("keyup", function (event) {
     input.value = "";
     showToDoItem("all");
     handleActiveFilterClass("all");
-
     localStorage.setItem("ToDo-Items", JSON.stringify(items));
   }
 });
@@ -42,16 +38,12 @@ input.addEventListener("keyup", function (event) {
 function showToDoItem(filter) {
   if (items.length > 0) {
     var empty = document.getElementById("empty");
-
     if (empty) {
       empty.remove();
     }
-
     clearBtn.classList.add("active-btn");
     clearBtn.removeAttribute("disabled");
-
     var li = "";
-
     items.forEach(function (item, index) {
       if (item.todoStatus === filter || filter === "all") {
         if (item.todoStatus === "completed") {
@@ -79,7 +71,6 @@ function showToDoItem(filter) {
               `;
       }
     });
-
     resultContainer.innerHTML = li;
   } else {
     var li = `
@@ -87,7 +78,6 @@ function showToDoItem(filter) {
             `;
     resultContainer.innerHTML = li;
   }
-
   if (body[0].offsetWidth > 480) {
     if (resultContainer.offsetHeight >= 164) {
       resultContainer.classList.add("scroll");
@@ -107,7 +97,6 @@ showToDoItem("all");
 
 function updateToDoItem(event, index) {
   var label = event.target.nextElementSibling;
-
   if (items[index].todoStatus === "pending") {
     items[index].todoStatus = "completed";
     label.classList.add("checked");
@@ -115,7 +104,6 @@ function updateToDoItem(event, index) {
     items[index].todoStatus = "pending";
     label.classList.remove("checked");
   }
-
   localStorage.setItem("ToDo-Items", JSON.stringify(items));
 }
 
@@ -127,11 +115,9 @@ all.addEventListener("click", function () {
 pending.addEventListener("click", function () {
   showToDoItem("pending");
   handleActiveFilterClass("pending");
-
   var isPending = items.every(function (item) {
     return item.todoStatus !== "pending";
   });
-
   if (isPending == true) {
     var liTag = `<li id="empty">You don't have any task here</li>`;
     resultContainer.innerHTML = liTag;
@@ -141,11 +127,9 @@ pending.addEventListener("click", function () {
 completed.addEventListener("click", function () {
   showToDoItem("completed");
   handleActiveFilterClass("completed");
-
   var isPending = items.every(function (item) {
     return item.todoStatus !== "completed";
   });
-
   if (isPending == true) {
     var liTag = `<li id="empty">You don't have any task here</li>`;
     resultContainer.innerHTML = liTag;
@@ -156,10 +140,8 @@ clearBtn.addEventListener("click", function () {
   items = [];
   showToDoItem("all");
   handleActiveFilterClass("all");
-
   clearBtn.classList.remove("active-btn");
   clearBtn.setAttribute("disabled", "");
-
   localStorage.setItem("ToDo-Items", JSON.stringify(items));
 });
 
@@ -167,21 +149,16 @@ function deleteToDoItem(index) {
   items.splice(index, 1);
   showToDoItem("all");
   handleActiveFilterClass("all");
-
   var firstLiTag = document.getElementById("item-0");
   if (!firstLiTag) {
     clearBtn.classList.remove("active-btn");
     clearBtn.setAttribute("disabled", "");
   }
-
   localStorage.setItem("ToDo-Items", JSON.stringify(items));
 }
 
 function editToDoItem(event, index) {
-  var labelValue =
-    event.target.parentElement.parentElement.children[0].children[1]
-      .textContent;
-
+  var labelValue = event.target.parentElement.parentElement.children[0].children[1].textContent;
   itemsId = index;
   input.value = labelValue;
   input.focus();
